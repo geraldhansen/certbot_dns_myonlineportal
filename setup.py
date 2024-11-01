e80b7d933867dcd1b278be92ed0d48bb6704c594
Gerald Hansen <gerald.hansen@posteo.de>
2024-11-01 21:48:26 +0100

    Fix naming for README.md
    Signed-off-by: Gerald Hansen <gerald.hansen@posteo.de>

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
-with open("Readme.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.0.2"
+version = "0.1.0"
 
 install_requires = [
     "acme>=0.29.0",
